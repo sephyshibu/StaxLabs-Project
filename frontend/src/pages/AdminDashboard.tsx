2b979f474408a91b0293d3d0736b138076f5a185
sephyshibu <sephyshibu@gmail.com>
2025-06-23 23:18:03 +0530

    logout button created for admin side

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -2,15 +2,34 @@ import { Tab } from '@headlessui/react';
 import FetchUsers from './FetchUser';
 import FetchOrders from './FetchOrder';
 import FetchProducts from './FetchProducts';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import {persistor}from '../../components/app/store'
+import { clearCredentials } from '../../components/features/AuthSlice';
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
 export default function AdminDashboard() {
+      const dispatch = useDispatch();
+      const navigate = useNavigate();
+
+    const handleLogout =async () => {
+        localStorage.clear(); // or remove only specific keys like localStorage.removeItem('token');
+         dispatch(clearCredentials());
+            await persistor.purge();
+            navigate('/login');
+    };
   return (
     <div className="max-w-6xl mx-auto p-6">
       <h1 className="text-3xl font-bold mb-6">Admin Dashboard</h1>
+        <button
+          onClick={handleLogout}
+          className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
+        >
+          Logout
+        </button>
 
       <Tab.Group>
         <Tab.List className="flex space-x-2 bg-blue-100 p-2 rounded-lg">
