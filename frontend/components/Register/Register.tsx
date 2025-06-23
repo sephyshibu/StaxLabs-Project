5f93b74070af85480625718a58b2c56e9cb4392d
sephyshibu <sephyshibu@gmail.com>
2025-06-23 22:14:14 +0530

    form validation for signup

--- frontend/components/Register/Register.tsx
+++ frontend/components/Register/Register.tsx
@@ -1,18 +1,77 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import API from '../Axios/axios';
+interface Signupform{
+    name:string,
+    email:string,
+    password:string,
+   role:string
+    
+
+}
 
 export default function Register() {
   const [form, setForm] = useState({ name: '', email: '', password: '', role: 'customer' });
+  const[loading,setloading]=useState(false)
+  const[error,seterror]=useState<Partial<Signupform>>({})
+  
   const navigate = useNavigate();
 
   const handleRegister = async () => {
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    seterror({})
+        setloading(true)
+
+        let formErrors:any={}
+        let isValid=true
+form
+        if (!form.name) {
+          formErrors.name = "Username is required";
+          isValid = false;
+      } else if (!/^[A-Za-z ]+$/.test(form.name)) {
+          formErrors.name = "Username must only contain letters and spaces.";
+          isValid = false;
+      }
+      
+        // Email validation (valid email format)
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if (!form.email) {
+            formErrors.email = 'Email is required.';
+            isValid = false;
+        } else if (!emailPattern.test(form.email)) {
+            formErrors.email = 'Please enter a valid email address.';
+            isValid = false;
+        }
+
+
+        // Password validation (at least 6 characters, one uppercase, one lowercase, one special character)
+         const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$/;
+        if (!form.password) {
+            formErrors.password = 'Password is required.';
+            isValid = false;
+        } else if (!passwordPattern.test(form.password)) {
+            formErrors.password = 'Password must be at least 6 characters long and include one uppercase letter, one lowercase letter, and one special character.';
+            isValid = false;
+        }
+      
+
+       
+
+        // If any validation fails, set error messages
+        seterror(formErrors);
+
+        // If any validation fails, return early
+        if (!isValid) {
+          setloading(false); 
+           return
+        }
     try {
       await API.post('/auth/register', {form,timezone});
       navigate('/login');
     } catch (err: any) {
       console.error('Registration failed:', err);
+       seterror(err.response?.data?.message ||"Something went wrong")   
     }
   };
 
@@ -24,17 +83,22 @@ export default function Register() {
         placeholder='Name'
         onChange={(e) => setForm({ ...form, name: e.target.value })}
       />
+       {error.name && <p className="text-red-500 text-center text-sm">{error.name}</p>}
+
       <input
         className="w-full border p-2 mb-3 rounded"
         placeholder='Email'
         onChange={(e) => setForm({ ...form, email: e.target.value })}
       />
+      {error.email && <p className="text-red-500 text-center text-sm">{error.email}</p>}
+
       <input
         className="w-full border p-2 mb-3 rounded"
         placeholder='Password'
         type='password'
         onChange={(e) => setForm({ ...form, password: e.target.value })}
       />
+                   {error.password && <p className="text-red-500 text-center text-sm">{error.password}</p>}
       <select
         className="w-full border p-2 mb-4 rounded"
         onChange={(e) => setForm({ ...form, role: e.target.value })}
@@ -43,6 +107,7 @@ export default function Register() {
         <option value='vendor'>Vendor</option>
         <option value='admin'>Admin</option>
       </select>
+
       <button className="w-full bg-green-500 text-white py-2 rounded" onClick={handleRegister}>
         Signup
       </button>
